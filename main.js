5a5aaaa6895672cea586543ecaf2cc9694d09844
Lily Hosein <26942736+naritaii@users.noreply.github.com>
2023-10-31 23:10:40 -0400

    update header js

--- main.js
+++ main.js
@@ -6,5 +6,13 @@
         .then(response => response.text())
         .then(data => {
             headerContainer.innerHTML = data;
+
+            // Highlight the link for the current page
+            const currentPage = window.location.pathname;
+            const links = headerContainer.querySelectorAll("a");
+            links.forEach(link => {
+                if (link.getAttribute("href") === currentPage) {
+                    link.classList.add("current-page");
+                }
+            });
         });
-  
