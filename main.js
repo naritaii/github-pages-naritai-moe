70571552f371e2bf883f98153f616a1fb2196a72
Lily Hosein <26942736+naritaii@users.noreply.github.com>
2023-10-31 22:56:58 -0400

    attempt fix links

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 // This script removes the .html extension from the URL
 if (location.pathname.endsWith(".html") && location.pathname !== "/index.html") {
     var newPath = location.pathname.replace(/.html$/, "");
+    if (newPath.endsWith("/")) {
+      newPath = newPath + "index.html";
+    } else {
+      newPath = newPath + ".html";
+    }
     window.history.replaceState({}, document.title, newPath);
   }
   
