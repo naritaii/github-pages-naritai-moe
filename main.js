fe65c05b50edee61a82ec793738da6c497276187
Lily Hosein <26942736+naritaii@users.noreply.github.com>
2023-10-31 23:08:11 -0400

    header

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
+    // Use JavaScript to load and insert the header
+    const headerContainer = document.getElementById("header-placeholder");
+    const headerURL = "header.html";
 
+    fetch(headerURL)
+        .then(response => response.text())
+        .then(data => {
+            headerContainer.innerHTML = data;
+        });
   
