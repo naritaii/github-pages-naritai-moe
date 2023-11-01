e72319f5fbdbe5795fdbf134d59bb54915c09873
Lily Hosein <26942736+naritaii@users.noreply.github.com>
2023-10-31 22:59:35 -0400

    doesnt work on github pages

--- main.js
+++ main.js
@@ -1,11 +1,2 @@
-// This script removes the .html extension from the URL
-if (location.pathname.endsWith(".html") && location.pathname !== "/index.html") {
-    var newPath = location.pathname.replace(/.html$/, "");
-    if (newPath.endsWith("/")) {
-      newPath = newPath + "index.html";
-    } else {
-      newPath = newPath + ".html";
-    }
-    window.history.replaceState({}, document.title, newPath);
-  }
+
   
